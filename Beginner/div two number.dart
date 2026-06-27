74e386e0c1f96d271d553e15ea122c5a7a25e7ac
Sohail <sohail0766@gmail.com>
2026-06-27 15:10:12 +0500

    Q6: div two numbers

--- Beginner/div two number.dart
+++ Beginner/div two number.dart
@@ -7,6 +7,6 @@ void main() {
   number1 = int.parse(stdin.readLineSync()!);
   stdout.write("Enter second number: ");
   number2 = int.parse(stdin.readLineSync()!);
-  div = number1 / number2;
+  div = number1 ~/ number2;
   print("The div of $number1 and $number2 is $div.");
 }

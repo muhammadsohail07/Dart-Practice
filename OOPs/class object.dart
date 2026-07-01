c03ef3f7ebfdd51329a0e1ccc005557f8ce7bd61
Sohail <anonymousx0766@gmail.com>
2026-07-01 15:21:34 +0500

    Class and Object

--- OOPs/class object.dart
+++ OOPs/class object.dart
@@ -2,19 +2,25 @@ class details {
   //class name
   String? name; // property of class
   int? age;
+
   void display() {
     // menthod or function of class
     print("Name: $name");
     print("Age: $age");
   }
+
+  details(String? city) {
+    // constructor of class
+    print("Constructor is called");
+  }
 }
 
 void main() {
-  details details1 = details(); // object of class
+  details details1 = details("New York"); // object of class
   details1.age = 22;
   details1.name = "Sohail";
   details1.display();
-  details details2 = details(); // another object of a class
+  details details2 = details("Los Angeles"); // another object of a class
   details2.age = 23;
   details2.name = "Faizan";
   details2.display();

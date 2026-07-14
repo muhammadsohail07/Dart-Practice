d41e8e1977320974d9188c002c794a262f3482fd
Sohail <anonymousx0766@gmail.com>
2026-07-14 22:41:35 +0500

    handle error by using try and catch in asynchronous function

---
+++ Asynchronous/handle error.dart
@@ -0,0 +1,18 @@
+main() {
+  print("Start");
+  getData();
+  print("End");
+}
+
+void getData() async {
+  try {
+    String data = await middleFunction();
+    print(data);
+  } catch (err) {
+    print("Some error $err");
+  }
+}
+
+Future<String> middleFunction() {
+  return Future.delayed(Duration(seconds: 5), () => "Hello");
+}

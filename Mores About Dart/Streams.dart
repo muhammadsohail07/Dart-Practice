623b7b82cb8730f44a262702f8bd6afc6242d6f4
Sohail <anonymousx0766@gmail.com>
2026-07-19 22:42:23 +0500

    stream in dart

---
+++ Mores About Dart/Streams.dart
@@ -0,0 +1,12 @@
+Stream<int> countStream(int max) async* {
+  for (int i = 1; i <= max; i++) {
+    await Future.delayed(Duration(milliseconds: 500));
+    yield i;
+  }
+}
+
+void main() async {
+  await for (int value in countStream(5)) {
+    print(value);
+  }
+}

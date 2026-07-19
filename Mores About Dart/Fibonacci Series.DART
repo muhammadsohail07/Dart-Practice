a35037dacbeb7331ee6d7c278fac8d5d410acc4a
Sohail <anonymousx0766@gmail.com>
2026-07-19 22:37:13 +0500

    fibonacci serirs

---
+++ Mores About Dart/Fibonacci Series.DART
@@ -0,0 +1,10 @@
+int fibonacci(int n) {
+  if (n <= 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+void main() {
+  for (int i = 0; i < 10; i++) {
+    print(fibonacci(i));
+  }
+}

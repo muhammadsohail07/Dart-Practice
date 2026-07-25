ec11b26e3375e98da42040b9d46a1d7a8cd0b6a0
Sohail <anonymousx0766@gmail.com>
2026-07-25 23:35:32 +0500

    bubble sort

---
+++ algorithms/Bubble Sort.dart
@@ -0,0 +1,15 @@
+void main() {
+  List<int> arr = [5, 2, 8, 1, 3];
+
+  for (int i = 0; i < arr.length - 1; i++) {
+    for (int j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        int temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  print(arr);
+}

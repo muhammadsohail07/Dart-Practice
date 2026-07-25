f206aaa692ea69296b3ae36a7c485bdbacfd1d75
Sohail <anonymousx0766@gmail.com>
2026-07-25 23:45:09 +0500

    insertion sort

---
+++ algorithms/Insertion Sort.dart
@@ -0,0 +1,17 @@
+void main() {
+  List<int> arr = [5, 2, 8, 1, 3];
+
+  for (int i = 1; i < arr.length; i++) {
+    int key = arr[i];
+    int j = i - 1;
+
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+
+    arr[j + 1] = key;
+  }
+
+  print(arr);
+}

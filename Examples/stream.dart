3f1cde123c4ecb17e684b302a85f0ede670e844d
Sohail <anonymousx0766@gmail.com>
2026-07-28 15:58:27 +0500

    stream

---
+++ Examples/stream.dart
@@ -0,0 +1,12 @@
+Stream<int> countStream() async* {
+  for (int i = 1; i <= 5; i++) {
+    await Future.delayed(Duration(seconds: 1));
+    yield i;
+  }
+}
+
+void main() async {
+  await for (var value in countStream()) {
+    print(value);
+  }
+}
